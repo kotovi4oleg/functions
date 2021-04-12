cc2e2c3c358bb2b8ec3bc900777b058f7290908e
kotovi4oleg <kotovi4.oleg@gmail.com>
2021-04-12 17:36:45 +0300

    Authoirzation Level change

--- src/FunctionApp/FunctionApp/Hello.cs
+++ src/FunctionApp/FunctionApp/Hello.cs
@@ -21,7 +21,7 @@ namespace FunctionApp
         [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
         public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
